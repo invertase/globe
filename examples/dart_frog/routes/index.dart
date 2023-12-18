c269717ddbe520170cdab0b2a73977f8f9089102
Salakar <mike@invertase.io>
2023-12-18 13:27:03 +0000

    chore: update cspell dict

--- examples/dart_frog/routes/index.dart
+++ examples/dart_frog/routes/index.dart
@@ -1,6 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
+  // ignore: avoid_print
   print('We have logs!');
   return Response(body: 'Welcome to Dart Frog running on Globe!');
 }
