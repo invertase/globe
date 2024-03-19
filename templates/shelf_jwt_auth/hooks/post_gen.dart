2277721bec19ad753c589f7573d40ee6b06bec47
Chima Precious <ositachima.co@gmail.com>
2024-03-19 22:02:47 +0000

    add post setup hook to install & setup project

---
+++ templates/shelf_jwt_auth/hooks/post_gen.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:mason/mason.dart';
+
+Future<void> run(HookContext context) async {
+  final progress = context.logger.progress('Setting up melos for workspace');
+
+  final p = await Process.run('melos', ['--version']);
+  if (p.pid != 0) {
+    await Process.run('dart', ['pub', 'global', 'activate', 'melos']);
+  }
+
+  final directoryName = context.vars['project_name'];
+  final actualPath = path.join(Directory.current.path, directoryName);
+
+  await Process.run(
+    'dart',
+    ["pub", "global", "run", "melos", "bootstrap"],
+    workingDirectory: actualPath,
+  );
+
+  progress.complete();
+}
