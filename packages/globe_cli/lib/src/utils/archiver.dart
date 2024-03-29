75b6b744f08d080a4a5541666d7732579f21a6c7
Chima Precious <ositachima.co@gmail.com>
2024-03-29 17:19:24 +0000

    fix: Rework gitignore matching (#71)
    * rework gitignore matching
    
    * ignore ignore.dart analyzer issues
    
    * include source

--- packages/globe_cli/lib/src/utils/archiver.dart
+++ packages/globe_cli/lib/src/utils/archiver.dart
@@ -2,10 +2,11 @@ import 'dart:io';
 
 import 'package:archive/archive_io.dart';
 import 'package:get_it/get_it.dart';
-import 'package:glob/glob.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:path/path.dart' as p;
 import 'package:pool/pool.dart';
+
+import 'ignore.dart';
 
 /// Creates a zip archive of the given [directory].
 Future<List<int>> zipDir(Directory directory) async {
@@ -25,106 +26,48 @@ Future<List<int>> zipDir(Directory directory) async {
       .where((entity) => entity is File && entity.path.endsWith('.gitignore'))
       .map((entity) => File(entity.path));
 
-  var gitignores = '';
+  final ignorePatterns = <String>[
+    '*.map',
+    '.git/**',
+    '.dart_tool/**',
+    '.packages',
+    '.idea/**',
+    '.vscode/**',
+    'build/**',
+    'android/**',
+    'ios/**',
+    'linux/**',
+    'macos/**',
+    'windows/**',
+  ];
 
   for (final file in gitignoreFiles) {
     if (file.existsSync()) {
       logger.detail('Found .gitignore file: ${file.path}');
-      gitignores += '${await file.readAsString()}\n';
+      final patterns = (await file.readAsLines()).where(
+        (e) => e.trim().isNotEmpty && !e.startsWith('#') && !e.startsWith('!'),
+      );
+      final dirname = p.dirname(file.path);
+      ignorePatterns.addAll(
+        patterns.map((pattern) {
+          pattern = pattern.startsWith('/')
+              ? dirname + pattern
+              : dirname + p.separator + pattern;
+          return pattern.replaceFirst(directoryPath, '');
+        }),
+      );
     }
   }
 
-  // Initialize two lists to hold exclusion and inclusion patterns
-  final excludePatterns = <String>[];
-  final includePatterns = <String>[];
-
-  gitignores
-      .split('\n')
-      .where((line) => line.isNotEmpty && !line.startsWith('#'))
-      .forEach((line) {
-    // Check if the line is an inclusion pattern (negation pattern)
-    final isInclusionPattern = line.startsWith('!');
-
-    if (isInclusionPattern) {
-      // Remove the leading '!' and process the line
-      line = line.substring(1);
-    }
-
-    // Process directory patterns by adding `**/` at the start if not already rooted and `/**` at the end
-    final isDirectory = line.endsWith('/');
-
-    // Remove leading `/` from the line if present.
-    var processedLine = line.startsWith('/') ? line.substring(1) : line;
-
-    if (isDirectory) {
-      // If a directory, prepend with `**/` and append with `/**` if it doesn't end with `/`.
-      processedLine =
-          "**/$processedLine${processedLine.endsWith('/') ? '' : '/**'}";
-    } else {
-      // For files, add `**/` at the start unless the pattern already starts with a specific directory
-      processedLine = '**/$processedLine';
-    }
-
-    // Add the processed line to the appropriate list
-    if (isInclusionPattern) {
-      includePatterns.add(processedLine);
-    } else {
-      excludePatterns.add(processedLine);
-    }
-  });
-
-  // Exclude common files.
-  excludePatterns.addAll([
-    '**/*.map',
-    '**/.git/**',
-    '**/.dart_tool/**',
-    '**/.packages',
-    '**/.idea/**',
-    '**/.vscode/**',
-    '**/build/**',
-    '**/android/**',
-    '**/ios/**',
-    '**/linux/**',
-    '**/macos/**',
-    '**/windows/**',
-  ]);
-
-  logger.detail('Excluding patterns: $excludePatterns');
-  logger.detail('Including patterns: $includePatterns');
-
   final pool = Pool(10);
+  final ignore = Ignore(ignorePatterns);
 
   Future<void> addEntityToArchive(FileSystemEntity entity) async {
     if (entity is! File) return;
 
-    // By default, don't exclude the entity.
-    var excludeEntity = false;
-
-    // Exclude files that match the patterns in the exclude list.
-    for (final pattern in excludePatterns) {
-      final glob = Glob(pattern);
-      final match = glob.matches(entity.path.replaceFirst(directoryPath, ''));
-      if (match) {
-        excludeEntity = true;
-        break;
-      }
-    }
-
-    // If the entity was marked for exclusion, check if it should be included again.
-    if (excludeEntity) {
-      for (final pattern in includePatterns) {
-        final glob = Glob(pattern);
-        final match = glob.matches(entity.path.replaceFirst(directoryPath, ''));
-        if (match) {
-          excludeEntity = false;
-          break;
-        }
-      }
-    }
-
-    // If excludeEntity is true after checking both lists, the entity should be excluded.
-    if (excludeEntity) {
-      logger.detail('Excluding file from archiving: ${entity.path}');
+    final shortPath = entity.path.replaceFirst(directoryPath, '');
+    if (ignore.ignores(shortPath)) {
+      logger.detail('Excluding file from archive: ${entity.path}');
       return; // Skip this entity, move to the next one
     }
 
