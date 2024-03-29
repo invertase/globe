b28d561ab92781ee1943acf0beab1abc67be384c
Chima Precious <ositachima.co@gmail.com>
2024-03-29 12:07:51 +0000

    use envied in frontend

--- templates/shelf_jwt_auth/__brick__/{{ project_name.snakeCase() }}/frontend/lib/data/services.dart
+++ templates/shelf_jwt_auth/__brick__/{{ project_name.snakeCase() }}/frontend/lib/data/services.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:frontend/data/api_service.dart';
+import 'package:frontend/env.dart';
 import 'package:get_it/get_it.dart';
 
 export 'api_service.dart';
@@ -7,11 +8,6 @@ export 'api_service.dart';
 final getIt = GetIt.instance;
 
 void setupServices() {
-  final apiEndpoint = const String.fromEnvironment(
-    'API_URL',
-    defaultValue: 'http://localhost:8080',
-  );
-
-  getIt.registerSingleton<ApiService>(ApiService(Uri.parse(apiEndpoint)));
+  getIt.registerSingleton<ApiService>(ApiService(Uri.parse(Env.apiURL)));
   getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
 }
---
+++ templates/shelf_jwt_auth/__brick__/{{ project_name.snakeCase() }}/frontend/lib/env.dart
@@ -0,0 +1,9 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'API_URL')
+  static const String apiURL = _Env.apiURL;
+}
