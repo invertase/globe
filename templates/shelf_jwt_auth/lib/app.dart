a6d442e17734c248b51a86a39e593abd82c87ef9
Chima Precious <ositachima.co@gmail.com>
2024-05-22 17:57:43 +0000

    chore: Add Shelf Jwt Auth Template (#83)
    * add shelf jwt auth template
    
    * add template doc
    
    * update template doc
    
    * update template doc
    
    * include templates dir in melos
    
    * Update templates/shelf_jwt_auth/lib/router.dart
    
    Co-authored-by: Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
    
    * Update templates/shelf_jwt_auth/lib/router.dart
    
    Co-authored-by: Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
    
    * _
    
    * _
    
    ---------
    
    Co-authored-by: Andrei Lesnitsky <andrei.lesnitsky@gmail.com>

---
+++ templates/shelf_jwt_auth/lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:dotenv/dotenv.dart';
+
+final env = DotEnv(includePlatformEnvironment: true)..load();
+final _secretKeyString = env['JWT_SECRET_KEY'] ??
+    (throw StateError('JWT_SECRET_KEY environment variable not provided'));
+final jwtSecretKey = SecretKey(_secretKeyString);
+
+typedef UserData = ({
+  String id,
+  String passwordHash,
+  DateTime createdAt,
+});
+
+// A map of username and userdata eg: { 'john-doe': (id, _, __) }
+final fauxUserDB = <String, UserData>{};
---
+++ templates/shelf_jwt_auth/lib/router.dart
@@ -0,0 +1,131 @@
+// Configure routes.
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:bcrypt/bcrypt.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+import 'app.dart';
+
+final bearerTokenRegExp = RegExp(r'Bearer (?<token>.+)');
+
+final router = Router()
+  ..post('/register', _register)
+  ..post('/login', _login)
+  ..get('/me', _getCurrentUser);
+
+Middleware authMiddleware = (innerHandler) {
+  return (request) {
+    final reqPath = request.url.path;
+
+    // Bypass auth header check for these routes
+    if (const ['login', 'register'].contains(reqPath)) {
+      return innerHandler(request);
+    }
+
+    final authHeader = request.headers[HttpHeaders.authorizationHeader] ?? '';
+    final match = bearerTokenRegExp.firstMatch(authHeader);
+    final jwt = match?.namedGroup('token');
+    if (jwt == null) {
+      return Response.unauthorized(null);
+    }
+
+    // Verify JWT token using secret key
+    final decodedToken = JWT.tryVerify(jwt, jwtSecretKey);
+    if (decodedToken == null) {
+      return Response.unauthorized(null);
+    }
+
+    return innerHandler(request.change(
+      context: {'user': decodedToken.payload},
+    ));
+  };
+};
+
+Response _getCurrentUser(Request request) {
+  final {
+    'username': String username,
+  } = Map<String, dynamic>.from(request.context['user']! as dynamic);
+  final userData = fauxUserDB[username]!;
+
+  return Response.ok(
+    jsonEncode({
+      'id': userData.id,
+      'username': username,
+      'createdAt': userData.createdAt.toIso8601String()
+    }),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+  );
+}
+
+Future<Response> _register(Request request) async {
+  ({String username, String password}) reqBody;
+
+  try {
+    final body =
+        await json.decode(await request.readAsString()) as Map<String, dynamic>;
+    reqBody = switch (body) {
+      {'username': String username, 'password': String password} => (
+          username: username,
+          password: password,
+        ),
+      _ => throw FormatException('Username & Password required for register'),
+    };
+  } on FormatException catch (e) {
+    return Response.badRequest(body: e.message);
+  }
+
+  if (fauxUserDB.containsKey(reqBody.username)) {
+    return Response.badRequest(body: 'Username already taken.');
+  }
+
+  final hashedPassword = BCrypt.hashpw(reqBody.password, BCrypt.gensalt());
+  final now = DateTime.now().toUtc();
+  fauxUserDB[reqBody.username] = (
+    passwordHash: hashedPassword,
+    createdAt: now,
+    id: '${now.toIso8601String()}_${fauxUserDB.length}',
+  );
+
+  return Response.ok('User registered');
+}
+
+Future<Response> _login(Request request) async {
+  ({String username, String password}) reqBody;
+
+  try {
+    final bodyString = await request.readAsString();
+    final body = json.decode(bodyString) as Map<String, dynamic>;
+    reqBody = switch (body) {
+      {'username': String username, 'password': String password} => (
+          username: username,
+          password: password,
+        ),
+      _ => throw FormatException('Username & Password required for register'),
+    };
+  } on FormatException catch (e) {
+    return Response.badRequest(body: e.message);
+  }
+
+  final userFromDB = fauxUserDB[reqBody.username];
+
+  final passwordMatches = userFromDB == null
+      ? false
+      : BCrypt.checkpw(reqBody.password, userFromDB.passwordHash);
+  if (!passwordMatches) {
+    return Response.unauthorized('Invalid user credentials');
+  }
+
+  // Issue a token valid for a day
+  final jwtToken = JWT({'username': reqBody.username}).sign(
+    jwtSecretKey,
+    expiresIn: const Duration(days: 1),
+  );
+
+  return Response.ok(
+    jsonEncode({'token': jwtToken}),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+  );
+}
