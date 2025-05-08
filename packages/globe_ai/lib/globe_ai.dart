0b9d8840781b0801fed9ef3e799d8272167515aa
Chima Precious <ositachima.co@gmail.com>
2025-05-08 09:08:25 +0000

    chore: export generate types for globe_ai

--- packages/globe_ai/lib/globe_ai.dart
+++ packages/globe_ai/lib/globe_ai.dart
@@ -5,5 +5,6 @@ import 'package:globe_ai/src/globe_ai_base.dart';
 
 export 'src/globe_ai_base.dart';
 export 'package:luthor/luthor.dart';
+export 'package:globe_ai/generated/openai.pbserver.dart';
 
 final openai = OpenAI(OpenAIConfig());
