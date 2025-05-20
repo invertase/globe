a2319f930be4424409f145f2f1b4bf4fd3832ef5
Jamiu Okanlawon <developerjamiu@gmail.com>
2025-05-20 15:36:06 +0100

    chore: add cors header to crud_rest_api_shelf template

--- templates/crud_rest_api_shelf/bin/server.dart
+++ templates/crud_rest_api_shelf/bin/server.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'package:crud_rest_api_shelf/routes/repository_route.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 void main(List<String> args) async {
   // Create handler
   final handler = Pipeline()
       .addMiddleware(logRequests())
+      .addMiddleware(corsHeaders())
       .addHandler(RepositoryRoute().router.call);
 
   // Start server
