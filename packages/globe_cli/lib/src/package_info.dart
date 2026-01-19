9c5c3128fe423b7e96ac61a0dd7343826f9bacf6
Chima Precious <ositachima.co@gmail.com>
2026-01-19 11:28:47 +0000

    chore(release): publish packages - globe_cli@0.0.16+4

--- packages/globe_cli/lib/src/package_info.dart
+++ packages/globe_cli/lib/src/package_info.dart
@@ -1,7 +1,7 @@
 // GENERATED CODE. DO NOT EDIT BY HAND.
 
 const name = 'globe_cli';
-const version = '0.0.16+3';
+const version = '0.0.16+4';
 const description =
     'The global deployment platform for Dart & Flutter applications.';
 const executable = 'globe';
